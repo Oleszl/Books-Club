57770c1c0990e70abba7a8ff2096256152b4267a
Oleszl <oles.sukmanovskyi@gmail.com>
2023-04-11 17:01:30 +0300

    initial commit

---
+++ src/main/java/com/excellentbook/excellentbook/ExcellentBookApplication.java
@@ -0,0 +1,13 @@
+package com.excellentbook.excellentbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcellentBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcellentBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellentbook/excellentbook/ExcellentBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellentbook.excellentbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcellentBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
